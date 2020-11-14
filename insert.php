2de3db318b0f39764d5c72e90a90e784085f14e1
Jeremy <than2314@gmail.com>
2020-11-14 23:34:57 +0700

    update aasdasd

--- insert.php
+++ insert.php
@@ -1,7 +1,7 @@
 <?php
 
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'itflabdb.mysql.database.azure.com', 'thetimes@itflab', 'PooMlmp99', 'itflab', 3306);
+mysqli_real_connect($conn, 'itflabdb.mysql.database.azure.com', 'thetimes@itflabdb', 'PooMlmp99', 'itflab', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
