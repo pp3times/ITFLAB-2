45e18cdff5724a4fb75620c83b6a54e6a3ce9be6
Jeremy <than2314@gmail.com>
2020-11-15 14:40:06 +0700

    edit

--- show.php
+++ show.php
@@ -149,7 +149,7 @@ while($Result = mysqli_fetch_array($res))
             <td><?php echo $Result['tel'];?></td>
             <td><?php echo $Result['comment'];?></td>
             <td><button type="button" class="btn btn-success editbtn">Edit</button></td>
-            <td><button type="button" class="btn btn-danger">Delete</button></td>
+            <td><button type="button" class="btn btn-danger editbtn" data-target="#editmodal">Delete</button></td>
             </tr>
             <?php
 }
@@ -230,6 +230,71 @@ mysqli_close($conn);
   </div>
 </div>
 <!-- End -->
+
+
+
+
+<!-- Modal -->
+<div class="modal fade" id="editmodal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+    <form action = "insert.php" method = "post" id="CommentForm" >
+       <div class="form-group">
+    <label for="exampleInputusername">Username</label>
+    <input type="text" class="form-control" name = "name" id="idName" placeholder="Thanawat Jantawong" required>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" name="mail" aria-describedby="emailHelp" placeholder="xxxxx@example.com" required>
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label for="inputPassword5">Password</label>
+<input type="password" id="inputPassword5" class="form-control" name="password" aria-describedby="passwordHelpBlock" required>
+<small id="passwordHelpBlock" class="form-text text-muted">
+  Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
+</small>
+  </div>
+ <div class="form-group">
+    <label for="rate">Rate us!</label>
+<input type="text" name = "comment" id="idComment" class="form-control" name="comment">
+<small class="form-text text-muted">
+  Tell me ypur opinion about my site.⚡️
+</small>
+  </div>
+
+<div class="form-group">
+    <label for="tel">Tel</label>
+<input type="tel" name = "tel" id="idtel" class="form-control" pattern="+[0-9]{4}-[0-9]{3}-[0-9]{4}"
+       required>
+<small class="form-text text-muted">
+  +66xx-xxx-xxxx
+</small>
+  </div>
+
+  <div class="form-group form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1" required>
+    <label class="form-check-label" for="exampleCheck1">I agree to the Terms and Condition ( beta )</label>
+  </div>
+  <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-primary" name="btn-save" id="commentBtn">Add user</button>
+      </div>
+</form>
+    </div>
+    </div>
+  </div>
+</div>
+<!-- End -->
+
+
 
 
 <script>
