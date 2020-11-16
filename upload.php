d5492107c30fc6daea230e97d936af9897d99fcb
Jeremy <than2314@gmail.com>
2020-11-16 13:41:19 +0700

    qweqwe

--- upload.php
+++ upload.php
@@ -2,23 +2,21 @@
 	$conn = mysqli_connect('itflabdb.mysql.database.azure.com', 'thetimes@itflabdb', 'PooMlmp99', 'itflab');
 
 	$name = $_POST['name'];
-    $comment = $_POST['comment'];
-    $mail = $_POST['mail'];
-    $password = $_POST['password'];
-    $tel = $_POST['tel'];
+	$comment = $_POST['comment'];
+	$link = $_POST['link'];
 	$id = $_POST['id'];
 
-	$sql = 'UPDATE guestbook SET name = "'.$name.'", comment = "'.$comment.'", mail = "'.$mail.'", password = "'.$password'", tel = "'.$tel'" WHERE ID = '.$id.'';
+	$sql = 'UPDATE guestbook SET Name = "'.$name.'", Comment = "'.$comment.'", Link = "'.$link.'" WHERE ID = '.$id.'';
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>ITF LAB</title>
+	<title>Edit | 13th ITF LAB</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
 	<style type="text/css">
 		.card {
-			border-top: solid 5px #000;
+			border-top: solid 5px #ffc107;
 		}
 	</style>
 </head>
@@ -38,7 +36,7 @@
 							}
 						?>
 						</h2>
-						<p align="center" class="mt-4 mb-0"><a href="show.php" class="btn btn-sm btn-warning">BACK</a></p>
+						<p align="center" class="mt-4 mb-0"><a href="index.php" class="btn btn-sm btn-warning">BACK</a></p>
 					</div>
 				</div>
 			</div>
@@ -46,3 +44,4 @@
 	</div>
 </body>
 </html>
+
