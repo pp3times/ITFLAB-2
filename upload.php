bc6455bea8ffac97561419d76db15fb4b52353c2
Jeremy <than2314@gmail.com>
2020-11-16 14:41:32 +0700

    adsadww

--- upload.php
+++ upload.php
@@ -2,7 +2,6 @@
 	$conn = mysqli_connect('itflabdb.mysql.database.azure.com', 'thetimes@itflabdb', 'PooMlmp99', 'itflab');
 
 	$name = $_POST['name'];
-<<<<<<< HEAD
 	$mail = $_POST['mail'];
 	$password = $_POST['password'];
 	$comment = $_POST['comment'];
@@ -10,13 +9,6 @@
 	$id = $_POST['id'];
 
 	$sql = 'UPDATE guestbook SET name = "'.$name.'", mail = "'.$mail.'",password = "'.$password.'", comment = "'.$comment.'", tel = "'.$tel.'" WHERE ID = '.$id.'';
-=======
-	$comment = $_POST['comment'];
-	$link = $_POST['link'];
-	$id = $_POST['id'];
-
-	$sql = 'UPDATE guestbook SET Name = "'.$name.'", Comment = "'.$comment.'", Link = "'.$link.'" WHERE ID = '.$id.'';
->>>>>>> main
 ?>
 <!DOCTYPE html>
 <html>
@@ -41,8 +33,12 @@
 							if(mysqli_query($conn, $sql)) {
 								echo "EDIT COMPLETED";
 							}
-							else {
-								echo "FAILED TO EDIT";
+							elseif(mysqli_query($conn)){
+								echo "FAILED TO load db";
+							}
+							else 
+								if(mysqli_query($sql)){
+									echo "Fail to load sql";
 							}
 						?>
 						</h2>
