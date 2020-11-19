11117d71044778a3f1e2d7775056b4d1278b85c2
pp3times <73677600+pp3times@users.noreply.github.com>
2020-11-19 20:16:05 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -34,7 +34,13 @@ if (mysqli_query($conn, $sql)) {
     ?>
         <div class="container">
 	   <div class="alert alert-info">
-        Success
+		   <div class="card">
+      <div style="border-radius:200px; height:200px; width:200px; background: #F8FAF5; margin:0 auto;">
+        <i class="checkmark">✓</i>
+      </div>
+        <h1>Success</h1> 
+        <p>We received your purchase request;<br/> we'll be in touch shortly!</p>
+      </div>
 		   <script>
 			   window.location.replace("show.php");
 		   </script>
