06b4490a899364b5a8be39a1b8a12584975cbe70
pp3times <73677600+pp3times@users.noreply.github.com>
2020-11-19 20:28:15 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -19,8 +19,10 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
 	<style type="text/css">
-		.card {
-			border-top: solid 5px #ffc107;
+		.box {
+			border: 1px solid;
+			padding: 10px;
+			box-shadow: 5px 10px;
 		}
 	</style>
 </head>
@@ -28,10 +30,10 @@
 	<div class="container text-dark">
 		<div class="row">
 			<div class="col-12 col-lg-8 offset-lg-2">
-				<div class="card shadow">
-					<div class="card-body">
+				<div class="box shadow">
+					<div class="box-body">
 						<div class="row">
-							<div class="col-6"><h1 class="text-monospace">Edit</h1></div>
+							<div class="col-6" aligh="center"><h1 class="font-weight-light">User edit</h1></div>
 						</div>
 						<form action="upload.php" method="post">
 							<input type="text" name="id" value="<?php echo $data['ID']; ?>" class="form-control d-none" required>
