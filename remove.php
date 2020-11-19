8f4a812c13e9a8e4484aedb75fb64a11f5f75223
pp3times <73677600+pp3times@users.noreply.github.com>
2020-11-19 20:20:33 +0700

    Update remove.php

--- remove.php
+++ remove.php
@@ -8,11 +8,41 @@
 	<title>Delete | 13th ITF LAB</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
-	<style type="text/css">
-		.card {
-			border-top: solid 5px #ffc107;
-		}
-	</style>
+	<link href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,400i,700,900&display=swap" rel="stylesheet">
+	<style>
+      body {
+        text-align: center;
+        padding: 40px 0;
+        background: #EBF0F5;
+      }
+        h1 {
+          color: #88B04B;
+          font-family: "Nunito Sans", "Helvetica Neue", sans-serif;
+          font-weight: 900;
+          font-size: 40px;
+          margin-bottom: 10px;
+        }
+        p {
+          color: #404F5E;
+          font-family: "Nunito Sans", "Helvetica Neue", sans-serif;
+          font-size:20px;
+          margin: 0;
+        }
+      i {
+        color: #9ABC66;
+        font-size: 100px;
+        line-height: 200px;
+        margin-left:-15px;
+      }
+      .card {
+        background: white;
+        padding: 60px;
+        border-radius: 4px;
+        box-shadow: 0 2px 3px #C8D0D8;
+        display: inline-block;
+        margin: 0 auto;
+      }
+    </style>
 </head>
 <body class="bg-light py-5">
 	<div class="container text-dark">
@@ -23,7 +53,15 @@
 						<h2 align="center">
 						<?php
 							if(mysqli_query($conn, $sql)) {
-								echo "DELETE COMPLETED";
+								?>
+							<div class="card">
+      <div style="border-radius:200px; height:200px; width:200px; background: #F8FAF5; margin:0 auto;">
+        <i class="checkmark">✓</i>
+      </div>
+        <h1>Success</h1> 
+        <p>Remove complate!</p>
+      </div>
+							<?php
 							}
 							else {
 								echo "FAILED TO DELETE";
