f0fbc6f7fb2e03ff5667daefbc20f078487cc515
Jeremy <than2314@gmail.com>
2021-02-08 23:13:44 +0700

    update file

--- edit.php
+++ edit.php
@@ -14,58 +14,70 @@
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
-	<title>ITF lab</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
-	<style type="text/css">
-		.box {
-			border: 1px solid;
-			padding: 10px;
-			box-shadow: 5px 10px;
-		}
-	</style>
+    <title>ITF lab</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
+    <style type="text/css">
+    .box {
+        border: 1px solid;
+        padding: 10px;
+        box-shadow: 5px 10px;
+    }
+    </style>
 </head>
+
 <body class="bg-light py-5">
-	<div class="container text-dark">
-		<div class="row">
-			<div class="col-12 col-lg-8 offset-lg-2">
-				<div class="box shadow">
-					<div class="box-body">
-						<div class="row">
-							<div class="col-6"><center><h1 class="font-weight-light">User edit</h1></center></div>
-						</div>
-						<form action="upload.php" method="post">
-							<input type="text" name="id" value="<?php echo $data['ID']; ?>" class="form-control d-none" required>
-							<div class="form-group">
-								<label>Name</label>
-								<input type="text" name="name" value="<?php echo $data['name']; ?>" class="form-control" required>
-							</div>
-							<div class="form-group">
-								<label>mail</label>
-								<input type="text" name="mail" value="<?php echo $data['mail']; ?>" class="form-control" required>
-							</div>
-							<div class="form-group">
-								<label>password</label>
-								<input type="password" name="password" value="<?php echo $data['password']; ?>" class="form-control" required>
-							</div>
-							<div class="form-group">
-								<label>Comment</label>
-								<textarea name="comment" class="form-control" rows="5" required><?php echo $data['comment']; ?></textarea>
-							</div>
-							<div class="form-group">
-								<label>tel</label>
-								<input type="text" name="tel" value="<?php echo $data['tel']; ?>" class="form-control">
-							</div>
-							<div class="text-center">
-								<button type="submit" class="btn btn-sm btn-dark my-3 font-weight-ligh">Save</button>
-								<a class="btn btn-sm btn-light my-3 font-weight-ligh" href="show.php">Cancle</a>
-							</div>
-						</form>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+    <div class="container text-dark">
+        <div class="row">
+            <div class="col-12 col-lg-8 offset-lg-2">
+                <div class="box shadow">
+                    <div class="box-body">
+                        <div class="row">
+                            <div class="col-6">
+                                <center>
+                                    <h1 class="font-weight-light">User edit</h1>
+                                </center>
+                            </div>
+                        </div>
+                        <form action="upload.php" method="post">
+                            <input type="text" name="id" value="<?php echo $data['ID']; ?>" class="form-control d-none"
+                                required> 
+                            <div class="form-group">
+                                <label>Name</label>
+                                <input type="text" name="name" value="<?php echo $data['name']; ?>" class="form-control"
+                                    required>
+                            </div>
+                            <div class="form-group">
+                                <label>mail</label>
+                                <input type="text" name="mail" value="<?php echo $data['mail']; ?>" class="form-control"
+                                    required>
+                            </div>
+                            <div class="form-group">
+                                <label>password</label>
+                                <input type="password" name="password" value="<?php echo $data['password']; ?>"
+                                    class="form-control" required>
+                            </div>
+                            <div class="form-group">
+                                <label>Comment</label>
+                                <textarea name="comment" class="form-control" rows="5"
+                                    required><?php echo $data['comment']; ?></textarea>
+                            </div>
+                            <div class="form-group">
+                                <label>tel</label>
+                                <input type="text" name="tel" value="<?php echo $data['tel']; ?>" class="form-control">
+                            </div>
+                            <div class="text-center">
+                                <button type="submit" class="btn btn-sm btn-dark my-3 font-weight-ligh">Save</button>
+                                <a class="btn btn-sm btn-light my-3 font-weight-ligh" href="show.php">Cancle</a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
+
 </html>
